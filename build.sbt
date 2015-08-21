98ef0a05b6e4ced9ad8fa4a79e4a0b0f88083f5e
kevinmeredith <kevin.m.meredith@gmail.com>
2015-08-20 23:57:22 -0500

    first

---
+++ build.sbt
@@ -0,0 +1,3 @@
+val paradiseVersion = "2.1.0-M5"
+
+addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full)
---
+++ src/main/scala/net/Equals5.scala
@@ -0,0 +1,26 @@
+package net
+
+import scala.language.experimental.macros
+import scala.reflect.macros.Context
+
+case class Equals5(value: Int) {
+	require(value == 5)
+}
+
+object Equals5 {
+	implicit def wrapInt(n: Int): Equals5 = macro verifyIntEquals5
+
+	def verifyIntEquals5(c: Context)(n: c.Expr[Int]): c.Expr[Equals5] = {
+    import c.universe._
+
+    val tree = n.tree match {
+      case Literal(Constant(x: Int)) if x > 0 =>
+        q"_root_.net.Equals5($n)"
+      case Literal(Constant(x: Int)) =>
+        c.abort(c.enclosingPosition, s"$x != 0")
+      case _ => 
+        q"_root_.net.Equals5($n)"
+    }
+    c.Expr(tree)
+  }
+}
